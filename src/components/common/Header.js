3d42d0c82fe4fa35120ef27ca590758c6485f6b8
Babasanya <Babz@Babz-Macbook-Air.local>
2017-08-04 04:42:39 +0100

    Initialize app

---
+++ src/components/common/Header.js
@@ -0,0 +1,37 @@
+// import Libraries to make component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Create component
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 5,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// Make components available to other parts of the app
+export {Header};
---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from './Header';
+export * from './MyButton';
+export * from './Card';
+export * from './CardSection';
+export * from './Input';
+export * from './Spinner';
